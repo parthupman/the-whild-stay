7031881d9381af6fa531ed125b9f8fd83924b33e
parthupman <parthupman@gmail.com>
2023-10-15 21:57:48 +0530

    Building Bookings

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -5,11 +5,13 @@ import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
 import SortBy from "../../ui/SortBy";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
   const [searchParams] = useSearchParams();
   if (isLoading) return <Spinner />;
+  if (!cabins.length) return <Empty resourceName="cabins" />;
 
   //1)FILTER
   const filterValue = searchParams.get("discount") || "all";
